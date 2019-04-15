0c468a8adb7d8905da6780346afa26e2441dae37
jeh26033 <jeh26033@email.vccs.edu>
2019-04-15 09:44:23 -0400

    applied updates
    general updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import myDumbHead from './pixeljoe2.0.gif'
 import TypedReactDemo from './TypedReactDemo.js'
 
 
-
 class App extends Component {
   render() {
 
@@ -18,38 +17,47 @@ class App extends Component {
         <div className= "container-fluid">
 
           <div className= "row">
-            <div className= "text-center text col-lg-7 col-md-8 col-sm-12" id="intro">  
+            <div className= "text-center text col-lg-8 col-md-12 col-sm-12" id="intro">  
               <div className = "row">           
                 <h2 className="text-center"> Character Selection:</h2>
-
                 <hr className="hr" />
-                <div className="col-md-6 text">
-                 
-                <TypedReactDemo
-                  strings={[
-                        '<span id="joe" className="name">Joseph Harskamp</span>^200\n`<span>Class: Web Dev</span>`'
-                      ]}
-                />
- 
+                <div className="col-md-8 text">
+                  <TypedReactDemo strings={['<h1><span id="joe" className="name">Joseph\nHarskamp</span></h1>^200\n`<span>Class: Web Developer</span>`']}/>
                   <hr />                    
-                <h4 class="text-center">Credits: 1 </h4>
-                <div className="col-md-12 row text">
-                  <div className="col-md-4 col-sm-12"><a href="https://twitter.com/josephharskamp" rel="noopener noreferrer" target="_blank">Twitter|</a></div>              
-                  <div className="col-md-4 col-sm-12"><a href="https://www.linkedin.com/in/joseph-harskamp/" target="_blank">LinkedIn|</a></div>
-                  <div className="col-md-4 col-sm-12"><a href="https://github.com/jeh26033" target="_blank">Github</a></div>
-                </div>
-                <hr />
-              </div>
-              <div className='col-md-6'>
-                <img className=" rounded float-right d-block  img-fluid" idName='myBigDumbHead'altName="A pixel portrait" src={myDumbHead}/>
-              </div>
-              </div>
-                        
+                  <h4 class="text-center">Credits: 1 </h4>
+                  <div className="col-md-12 row text">
+                    <div className="col-md-4 col-sm-12"><a href="https://twitter.com/josephharskamp" rel="noopener noreferrer" target="_blank">Twitter</a></div>                  
+                    <div className="col-md-4 col-sm-12"><a href="https://www.linkedin.com/in/joseph-harskamp/" rel="noopener noreferrer" target="_blank">LinkedIn|</a></div>
+                    <div className="col-md-4 col-sm-12"><a href="https://github.com/jeh26033" rel="noopener noreferrer" target="_blank">Github</a></div>
+                  </div>
+                  <hr />
+                   <ul id="stats">
+                    <li>Class: Web Developer </li>
+                    <li>Main Weapon: Keyboard/mouse combo</li>
+                    <li>Special move: Hot Pocket Power-up</li>
+                    <li>Intelligence: <span class="triple oi oi-star" title="star" aria-hidden="true"></span><span class="triple oi oi-star" title="star" aria-hidden="true"></span><span class="triple oi oi-star" title="star" aria-hidden="true"></span></li>
 
-            </div> {/*text center*/}
-          </div> {/*row*/}
-        </div> {/*container*/}
-      </div>
+                    <li>Dexterity: fa("fort-awesome") <span class="double oi oi-star" title="star" aria-hidden="true"></span><span class="double oi oi-star" title="star" aria-hidden="true"></span></li>
+
+                    <li>Endurance: <span class="double oi oi-star" title="star" aria-hidden="true"></span><span class="double oi oi-star" title="star" aria-hidden="true"></span></li>
+
+                    <li>Strength: <span class="double oi oi-star" title="star" aria-hidden="true"></span><span class="double oi oi-star" title="star" aria-hidden="true"></span></li>
+
+
+                    
+                  </ul>
+                </div>
+                <div className='col-md-4'>
+                  <img className=" rounded img-fluid" idName='myBigDumbHead'altName="A pixel portrait" src={myDumbHead}/>
+                </div>
+              </div>
+
+            <div classsName='col-md-4'></div> {/*bio*/}
+
+          </div> {/*text center*/}
+        </div> {/*row*/}
+      </div> {/*container*/}
+    </div>
       
     ); 
   }
